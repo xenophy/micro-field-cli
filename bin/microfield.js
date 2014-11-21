882b8d82e1391c97f70a3b7f8aa2dac9881adddd
xenophy <kotsutsumi@xenophy.com>
2014-11-22 04:43:49 +0900

    CLIクラス作成開始

--- bin/microfield.js
+++ bin/microfield.js
@@ -1,4 +1,29 @@
 #!/usr/bin/env node
 
-var microfield  = require('../');
+/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
 
+/*
+This file is part of MicroField CLI
+
+Copyright (c) 2014 Xenophy.CO.,LTD All rights Reserved.
+http://www.xenophy.com
+*/
+
+(function() {
+
+    "use strict";
+
+    var CLI  = require('../');
+
+    console.log(CLI);
+
+
+})();
+
+/*
+ * Local variables:
+ * tab-width: 4
+ * c-basic-offset: 4
+ * c-hanging-comment-ender-p: nil
+ * End:
+ */

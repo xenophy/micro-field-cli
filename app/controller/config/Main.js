6d448c65bc239c83b238e49a79d81ec84dcf8ad6
xenophy <kotsutsumi@xenophy.com>
2014-12-05 15:42:18 +0900

    update

---
+++ app/controller/config/Main.js
@@ -0,0 +1,40 @@
+/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
+
+// {{{ MicroField.controller.config.Main
+
+CLI.define('MicroField.controller.config.Main', {
+
+    // {{{ extend
+
+    extend: 'MicroField.controller.Controller',
+
+    // }}}
+    // {{{ run
+
+    run: function() {
+
+
+
+
+
+        console.log("config");
+
+
+
+
+
+    }
+
+    // }}}
+
+});
+
+// }}}
+
+/*
+ * Local variables:
+ * tab-width: 4
+ * c-basic-offset: 4
+ * c-hanging-comment-ender-p: nil
+ * End:
+ */
